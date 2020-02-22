526aad34efb99e5d7a9e1a70ba6fcef93c114737
rbuerki <r2d4@bluewin.ch>
2020-02-22 21:34:13 +0100

    initial commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+# Loading collection raph
+# -----------------------
+
+# Path of collection data file (str, pointing to .xlsx file)
+collection_path = "data/dm_records_20-02-17.xlsx"
+
+# Columns from collection file to include in DataFrame (list)
+collection_cols = ["Artist", "Titel"]
+
+# Genre(s) to load (list or None, if all albums are to be loaded)
+collection_genres = ["Death Metal"]
