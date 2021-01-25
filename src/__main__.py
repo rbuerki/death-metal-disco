3363cfa759750f666bb067029abd64fbad1e13b5
rbuerki <r2d4@bluewin.ch>
2021-01-25 16:18:41 +0100

    Add regular addition function to __main__

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,16 @@
 from sqlalchemy.exc import ProgrammingError, OperationalError
 
 from src import db_connect
+from src import db_functions
 
 
 def main():
     engine, session = db_connect.main()
     print(engine)
     print(session)
-    pass
+
+    # Always check if Addition is up
+    db_functions.add_regular_credits(session)
     session.close()
 
 
