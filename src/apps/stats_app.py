ca73ca94418d7e29007f5f7d2500581da79b29e1
rbuerki <r2d4@bluewin.ch>
2021-02-17 14:26:56 +0100

    Clean up label names on PROD

--- src/apps/stats_app.py
+++ src/apps/stats_app.py
@@ -4,29 +4,29 @@ from src.db_declaration import CreditTrx
 
 
 # DEV SECTION
-import src.db_connect as db_connect
+# import src.db_connect as db_connect
 
 
-@st.cache(allow_output_mutation=True)
-def get_engine_and_scoped_session():
-    """Note: This function is defined in this module (and not in
-    db_connect) because of the caching option.
-    """
-    config_params = db_connect.read_yaml("config.yaml", "DB_PROD")
-    engine = db_connect.create_engine(config_params)
-    Session = db_connect.create_scoped_session(engine)
-    return engine, Session
+# @st.cache(allow_output_mutation=True)
+# def get_engine_and_scoped_session():
+#     """Note: This function is defined in this module (and not in
+#     db_connect) because of the caching option.
+#     """
+#     config_params = db_connect.read_yaml("config.yaml", "DB_PROD")
+#     engine = db_connect.create_engine(config_params)
+#     Session = db_connect.create_scoped_session(engine)
+#     return engine, Session
 
 
-engine, Session = get_engine_and_scoped_session()
-session = Session()
+# engine, Session = get_engine_and_scoped_session()
+# session = Session()
 
-result = (
-    session.query(CreditTrx.credit_saldo)
-    .order_by(CreditTrx.credit_trx_id.desc())
-    .first()[0]
-)
-st.write(f"Actual Credit Saldo: {result}")
+# result = (
+#     session.query(CreditTrx.credit_saldo)
+#     .order_by(CreditTrx.credit_trx_id.desc())
+#     .first()[0]
+# )
+# st.write(f"Actual Credit Saldo: {result}")
 
 
 # PROD SECTION
